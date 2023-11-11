2dbaaa2482682a33a02c52165cb9f0cd64006070
Fakhrul0121 <106474317+Fakhrul0121@users.noreply.github.com>
2023-11-11 20:51:17 +0700

    Initial commit

---
+++ src/main/java/WCO/Website/Congklak/Online/WebsiteCongklakOnlineApplication.java
@@ -0,0 +1,13 @@
+package WCO.Website.Congklak.Online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteCongklakOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteCongklakOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WCO/Website/Congklak/Online/WebsiteCongklakOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package WCO.Website.Congklak.Online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteCongklakOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
